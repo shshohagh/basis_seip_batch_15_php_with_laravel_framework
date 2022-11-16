32e303be46ec6a87a5ff14ce3ed293e4d79799a1
SKS IT FIRM <sksitfirm@gmail.com>
2022-11-16 06:32:09 +0600

    class-24

---
+++ class-24/resources/views/test.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <section class="mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Test</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
